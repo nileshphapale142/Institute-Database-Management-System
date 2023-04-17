f5255ddfa98f72f9601ff618b4c4eb5bd0ff2de1
Nilesh Phapale <nileshphapale142@gmail.com>
2023-04-17 19:22:51 +0530

    Improvised insert functionality

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const mysql = require('mysql')
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const flash = require('express-flash');
+const flash = require('connect-flash');
 const session = require('express-session');
+const {NULL} = require("mysql/lib/protocol/constants/types");
+const Errors = require("mysql/lib/protocol/constants/errors");
 
 const routers = express.Router()
 
 const app = express()
-
-
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -24,7 +24,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
     secret: '123456catr',
@@ -39,14 +38,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine','ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
-
-// let attrTypeArray
-
-// let setOutput = (rows) => {
-//     attrTypeArray = rows
-//     console.log(attrTypeArray)
-// }
-
 
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.resolve(__dirname, './public/index.html'))
@@ -72,6 +63,8 @@ app.post('/insert', (req, res) => {
 })
 
 app.post('/insert/table', async (req, res) => {
+    //TODO: Check for invalid data
+
     console.log(req.query)
     console.log(req.body)
     const table = req.query.name
@@ -81,6 +74,7 @@ app.post('/insert/table', async (req, res) => {
     for (let attr in tuple) {
         query += attr.toString() + ','
     }
+
     query = query.slice(0, query.length - 1)
     query += ') values('
     for (let attr in tuple) {
@@ -95,59 +89,55 @@ app.post('/insert/table', async (req, res) => {
         if (err) console.log('some error')
 
         let insertArray = []
+        let primaryKey = []
 
         for (let i = 0; i < rows.length; i ++) {
-            if (rows[i].Type.toString().slice(0, 3) === 'int')
-                insertArray.push(parseInt(tuple[rows[i].Field]))
+
+            const type = rows[i].Type.toString().slice(0, 3).toString()
+            if (type === 'int') {
+                if (isNaN(parseInt(tuple[rows[i].Field]))) {
+                    insertArray.push(null)
+                }
+                else
+                    insertArray.push(parseInt(tuple[rows[i].Field]))
+            }
+            else if (type === 'dec') {
+                if (isNaN(parseFloat(tuple[rows[i].Field])))
+                    insertArray.push(null)
+                else
+                    insertArray.push(parseFloat(tuple[rows[i].Field]))
+            } else if (type === 'dat') {
+                if (tuple[rows[i].Field] === '')
+                    insertArray.push(null)
+                else
+                    insertArray.push(tuple[rows[i].Field])
+            }
             else insertArray.push(tuple[rows[i].Field])
         }
+
 
         console.log(insertArray)
 
         connection.query(query, insertArray, (e, r, f) => {
-            if (e) console.log('some error in insertion : ', e)
-            // req.flash('success', 'Unic')
-            res.status(200).send('Successful')
+            if (e) {
+                console.log('some error in insertion : ', e)
+                const errorMsg = e.message.toString().split(' ')
+
+                if (errorMsg[0] === 'ER_DUP_ENTRY:') {
+                    let msg = errorMsg[1] + ' ' + errorMsg[2] + ' ' + errorMsg[3]
+                    for (let i = 4; i < errorMsg.length; i ++) {
+                        if(errorMsg[i] === 'for') break;
+                        msg += ' ' + errorMsg[i]
+                    }
+                    res.status(200).send(msg)
+                }
+                else res.status(200).send('Error 404: Problem while inserting data!')
+            } else
+                res.status(200).send('Successful')
         })
     })
 })
 
-
-// app.get('/insert/query', (req, res) => {
-//     const table = req.query.tableList
-//     connection.query('select * from ' + table, (err, rows, fields) => {
-//         if (err) throw err
-//
-//         res.status(200).render('insert', {
-//             temp: 'Enter the ' + table + ' information below',
-//             columns: fields
-//         })
-//     })
-//     // res.send('hello universe')
-// })
-
-// app.post('/insert/query/info', (req, res) => {
-//     console.log(req.body)
-//     res.send('Hello')
-// })
-
-app.post('/insert/query/some', function(req, res, next) {
-    // var f_name = req.body.f_name;
-    // var l_name = req.body.l_name;
-    // var email = req.body.email;
-    // var message = req.body.message;
-
-    console.log(req.body)
-    // console.log(f_name + ' ' + l_name + ' ' + email + ' ' + message)
-    res.redirect('/')
-    // var sql = `INSERT INTO contacts (f_name, l_name, email, message, created_at) VALUES ("${f_name}", "${l_name}", "${email}", "${message}", NOW())`;
-    // db.query(sql, function(err, result) {
-    //     if (err) throw err;
-    //     console.log('record inserted');
-    //     req.flash('success', 'Data added successfully!');
-    //     res.redirect('/');
-    // });
-});
 app.get('/see', (req, res) => {
 
     res.status(200).render('see', {
@@ -160,17 +150,25 @@ app.get('/see', (req, res) => {
 app.get('/see/query', (req, res) => {
     const table = req.query.tablelist
 
-        connection.query('select * from ' + table, (err, rows, fields) => {
-            if (err) throw err
-            res.status(200).render('see', {
-                name: table.toString().toLocaleUpperCase(),
-                someData: rows,
-                columns: fields
-            })
+    connection.query('select * from ' + table, (err, rows, fields) => {
+        if (err) throw err
+        res.status(200).render('see', {
+            name: table.toString().toLocaleUpperCase(),
+            someData: rows,
+            columns: fields
         })
-
+    })
 })
+
+app.get('/advance', (req, res) => {
+    res.status(200).send('Want advance option?')
+})
+
 app.get('/about', (req, res) => {
+    connection.query('desc student', (err, rows, fields) => {
+        if (err) console.log("Some error")
+        else console.log(rows)
+    })
     res.status(200).send('<p>You want to know about me?</p>')
 })
 
